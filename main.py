fdfb90be0ed854a933695f7e04266823b78cafcc
Benson Lindsey <blthree89@gmail.com>
2018-03-09 00:08:54 -0500

    add endpoint for tweetflow

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main_page():
     return render_template('index.html')
 
+@app.route('/tweetflow')
+def tweetflow():
+    return render_template('tweetflow.html')
+
 
 if __name__ == '__main__':
     app.run()
